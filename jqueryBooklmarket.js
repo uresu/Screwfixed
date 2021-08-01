10abd7f48301ac30074d06a0c620a83a51d13a84
Wes Thompson <wes.harris@gmail.com>
2021-08-01 11:15:47 +0100

    Update jqueryBooklmarket.js

--- jqueryBooklmarket.js
+++ jqueryBooklmarket.js
@@ -1,23 +1,23 @@
-javascript: (function() {
-    'use strict';
-
-    console.log("Screwfix Fix 0.1");
-
-    var dataLoadCallback = function(data)
-    {
-        console.log(this);
-        $('.targetStockValue').find('p:contains("in stock in")').parents("[id^=productID_]").css("background", "#cfc");
-    }
-
-    $("[id^=productID_]").each
+javascript: (() => {
+  
+  console.log("Screwfix Fix 0.1");
+  
+   jQuery("[id^=productID_]").each
     (
         function(i)
         {
             var counter = i.toString();
-            var url = $(this).find('a').attr('href');
+            var url = jQuery(this).find('a').attr('href');
             var liTag = '<li class="targetStockValue" id="targetStockValue_' + counter + '"><img src="https://i.imgur.com/UQ4XQlI.gif"></li>';
-            $(this).find('ul').append(liTag);
-            $('#targetStockValue_' + counter).load(url + ' [id^=branch_collection_]', dataLoadCallback);
+            jQuery(this).find('ul').append(liTag);
+            jQuery('#targetStockValue_' + counter).load(url + ' [id^=branch_collection_]', function(data)
+            {
+                console.log(this);
+                jQuery('.targetStockValue').find('p:contains("in stock in")').parents("[id^=productID_]").css("background", "#cfc");
+            });
         }
     );
 })();
+
+
+
